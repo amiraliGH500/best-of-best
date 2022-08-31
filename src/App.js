4548dbdaf6b4a324323c7c367ccc86fec3a88cfb
amiraliGH500 <amiralighasemi@yahoo.com>
2022-08-31 22:35:49 +0430

    noice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { blogList } from "./blog-list.js";
 import { motion, Variants } from "framer-motion";
-
 const imageAnimate={
     offscreen:{x:-100, opacity:0},
     onscreen:{
@@ -16,7 +15,7 @@ const imageAnimate={
 }
 
 const textAnimate={
-    offscreen:{y:100, opacity:0},
+    offscreen:{y:0, opacity:0},
     onscreen:{y:0,
     opacity:1,
     transition: {type:"spring",
@@ -26,9 +25,10 @@ const textAnimate={
 
 }
 
-function Card({ image, h2, p,id }) {
+function Card() {
   return (
-    <motion.div className="card" id={id}
+    <div>
+          <motion.div className="card"
         initial={"offscreen"}
         whileInView={"onscreen"}
         viewport={{once:false, amount:0.5}}
@@ -36,22 +36,139 @@ function Card({ image, h2, p,id }) {
     >
       <motion.div className="image-container"       
         variants={imageAnimate}
-      >{image}</motion.div>
+      >
+        <img src="mrB.png" className="size"/>
+        </motion.div>
       <motion.h2 
         variants={textAnimate}
-      >{h2}</motion.h2>
+      >Mr.Beast</motion.h2>
       <motion.p
          variants={textAnimate}     
-      >{p}</motion.p>
+      >React, Short, iconic, Challenge</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="mia.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Mia Plays</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >React, Games, Vlog</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="skeppy.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Skeppy</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >Minecraft, Troll, challenge</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="donji.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Doni Bobes</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >Minecraft, Troll, Challenge</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="dream.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Dream</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >Minecraft, Challenge</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="vigiato.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Vigiato</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >Minecraft, Challenge, Games</motion.p>
+    </motion.div>    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="sogang.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Sogang</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >React, Vlog</motion.p>
     </motion.div>
+    <motion.div className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >
+        <img src="sypher.png" className="size"/>
+        </motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >Sypher PK</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >Fortnite, React</motion.p>
+    </motion.div>
+    </div>
   );
 }
 
-export default function App() {
-  return blogList.map((item, index) => (
-      <div className="card-wrapper" key={index}>
-          <Card image={item.image} h2={item.h2} p={item.p}  />
-      </div>
-  ));
-}
+export default Card
 
